3f2c46a444da384f7b62ba039c8f66447aa64072
punitram <56621050+punitram@users.noreply.github.com>
2020-12-13 19:26:15 -0800

    Add files via upload

---
+++ NeonGamesDemo1/Assets/Scripts/RespawnLava.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class RespawnLava : MonoBehaviour
+{
+    private Vector3 spawnPoint = new Vector3(6, 12, 0);
+
+    void OnTriggerEnter(Collider coll)
+    {
+        if (coll.gameObject.CompareTag("Lava"))
+        {
+            transform.position = spawnPoint;
+        }
+    }
+}
